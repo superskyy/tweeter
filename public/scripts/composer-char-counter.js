601089f8be44428f4ccff3ed7dd1f37136666960
Peter <psynevskyy.com>
2019-05-14 20:59:22 +0000

    Character counter works

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $("textarea").keyup(function() {
+  	let Characters = 0;
+    Characters = $("textarea").val().length;
+    if (Characters > 140) {
+    $(".counter").text(140 - Characters).css({color: "red"});
+	} else {
+		$(".counter").text(140 - Characters).css({color: "black"});
+	}
+  });
+});
