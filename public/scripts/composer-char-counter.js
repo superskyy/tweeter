0a7accfeb8500e24cc8c40932591509164ec96d7
Peter <psynevskyy.com>
2019-05-14 21:29:26 +0000

    refactored the counter function

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,10 +2,10 @@ $(document).ready(function() {
   $("textarea").keyup(function() {
   	let Characters = 0;
     Characters = $("textarea").val().length;
-    if (Characters > 140) {
-    $(".counter").text(140 - Characters).css({color: "red"});
+   	if (Characters > 140) {
+      $(".counter").text(140 - Characters).css({color: "red"});
 	} else {
-		$(".counter").text(140 - Characters).css({color: "black"});
+	  $(".counter").text(140 - Characters).css({color: "black"});
 	}
   });
 });
